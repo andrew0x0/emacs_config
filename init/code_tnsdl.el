010975635bf852fd1c2343ba8b7299d7a0a355f3
andrew0x0 <andrew.guoyu@gmail.com>
2016-07-15 02:46:43 -0700

    remove zip files

--- init/code_tnsdl.el
+++
@@ -1,8 +0,0 @@
-;;tnsdl-mode
-(load "tnsdl")
-(define-key tnsdl-mode-map [(meta f1)] 'tnsdl-goto-block-start)
-(define-key tnsdl-mode-map [(meta f2)] 'tnsdl-goto-block-end)
-(define-key tnsdl-mode-map [(meta f3)] 'tnsdl-goto-proc-start)
-(define-key tnsdl-mode-map [(meta f4)] 'tnsdl-goto-proc-end)
-(define-key tnsdl-mode-map [(meta n)] 'tnsdl-forward-sexp)
-(define-key tnsdl-mode-map [(meta p)] 'tnsdl-backward-sexp)
