0b91fea18738fa11ee66a83a4e148229aff795b1
guoyu <guoyu@chainwis.cn>
2018-08-17 16:18:18 +0800

    add packet

--- init/a_code.el
+++ init/a_code.el
@@ -19,4 +19,73 @@
 	  ("melpa" . "http://mirrors.163.com/elpa/melpa/")))
 ;;      '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
 ;;        ("melpa" . "http://elpa.emacs-china.org/melpa/")))
-(package-initialize))
+  (package-initialize))
+
+;;; set up package syncing to allow for syncing between different machines
+
+;; list of packages to sync
+(setq pfl-packages
+      '(
+	company-anaconda
+	anaconda-mode
+	multiple-cursors
+	browse-kill-ring
+	xcscope
+	company
+	flycheck
+	flycheck-irony	
+	company-c-headers
+	ace-jump-mode
+	symbol-overlay
+	markdown-mode
+	company-tern	
+	company-irony
+	company-go
+	company-irony-c-headers
+	irony-eldoc
+	irony
+	counsel
+	ivy
+	wgrep
+	avy
+	smex
+	swiper
+	ivy-hydra
+	expand-region
+	counsel-etags
+	counsel-gtags	
+	ace-window
+	itail
+	lua-mode
+	hc-zenburn-theme
+	spacemacs-theme
+	))
+
+;; refresh package list if it is not already available
+(when (not package-archive-contents) (package-refresh-contents))
+
+;; install packages from the list that are not yet installed
+(dolist (pkg pfl-packages)
+  (if (not (package-installed-p pkg))
+      (if (assoc pkg package-archive-contents)
+	  (package-install pkg)
+	(progn   (package-refresh-contents)
+		 (package-install pkg)))))
+
+(defun reinstall-all-package ()
+  (interactive)
+  (package-refresh-contents)
+  (dolist (pkg pfl-packages)
+  (when (assoc pkg package-archive-contents)
+    (package-install pkg)))
+  )
+
+(setq my_use-anaconda nil)
+(setq my_use-rtags nil)
+(setq my_use-irony nil)
+
+(let ((have_rc (shell-command "which rc")))
+  (if (eq have_rc 0)
+      (progn
+        (setq my_use-rtags t)
+        (setq my_use-irony t))))
